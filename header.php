79cb39276c02631ebd34b1a0dc5ed7ffa169e1fe
Markus Reiter <me@reitermark.us>
2016-01-03 22:27:18 +0100

    Add Modernizr.

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <?php wp_head(); ?>
 
 <body <?php body_class(); ?>>
