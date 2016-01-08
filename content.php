427fdb97801f24cf15e6e7f560634ef3ecce8f8e
Markus Reiter <me@reitermark.us>
2016-01-08 21:32:11 +0100

    Simplify PHP files.

--- content.php
+++ content.php
@@ -2,9 +2,9 @@
 
   <?php
   /**
-    * @hooked et_shop_post_header() - 10
-    * @hooked et_shop_post_meta() - 20
-    * @hooked et_shop_post_content() - 30
+   * @hooked et_shop_post_header() - 10
+   * @hooked et_shop_post_meta() - 20
+   * @hooked et_shop_post_content() - 30
    */
   do_action('et_shop_loop_post');
   ?>
