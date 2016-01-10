a82052c5a1c3e293956d8bd25682bc675b780716
Markus Reiter <me@reitermark.us>
2016-01-10 08:51:41 +0100

    Add filters.

--- inc/functions/filters.php
+++ inc/functions/filters.php
@@ -6,3 +6,15 @@ function remove_size_attributes_from_image($img) {
 add_filter('post_thumbnail_html',  'remove_size_attributes_from_image');
 add_filter('image_send_to_editor', 'remove_size_attributes_from_image');
 add_filter('get_avatar',           'remove_size_attributes_from_image');
+
+
+function remove_formal_from_language_attributes($lang) {
+  return preg_replace('/(?<=[^-][^x])-formal(\'|")/', '$1', $lang);
+}
+add_filter('language_attributes',  'remove_formal_from_language_attributes');
+
+
+function remove_role_from_search_form($form) {
+  return preg_replace('/\srole=("search"|\'search\')/', '', $form);
+}
+add_filter('WC_Widget_Product_Search', 'remove_role_from_search_form');
